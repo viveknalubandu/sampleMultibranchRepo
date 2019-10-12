4fd3e3fb23248db21767fc02ba666ff4ac108227
Srilekha Sahoo <srilekha.sahoo@gmail.com>
2019-10-12 03:49:12 -0700

    reslove conflict

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,11 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-       
-<<<<<<< HEAD
+
         System.out.println( "Hello devops world dev branch111");
-=======
-        System.out.println( "Hello devops world3were223test");
->>>>>>> master
     }
 }
