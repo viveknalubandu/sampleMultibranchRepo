7b8601dd7bed77c7910e3c50dc0af43edf7ed3c6
Srilekha Sahoo <srilekha.sahoo@gmail.com>
2019-10-12 03:07:40 -0700

    rename test

--- src/test/java/com/sndevops/eng/App1Test.java
+++ src/test/java/com/sndevops/eng/App1Test.java
@@ -2,7 +2,7 @@ package com.sndevops.eng;
 import org.junit.Assert;
 import org.junit.Test; 
 
-public class AppTest1 {
+public class App1Test {
 // rrt   13411
     // Test
     private String INPUT = "13412";
